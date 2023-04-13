2a6375d6def1a6602bc8b0eec4eaf4c7a86d1fdc
Nitroceliulioze <aksionova.diana@gmail.com>
2023-04-13 14:56:44 +0300

    building form

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+
+  constructor(private fb: FormBuilder, private toastr: ToastrService, private service: AuthService, private router: Router) {}
+
+  registerForm = this.fb.group({
+    id: this.fb.control('', Validators.compose([Validators.required, Validators.minLength(5)])),
+    name: this.fb.control('', Validators.required),
+    password: this.fb.control('', Validators.compose([Validators.required, Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$")])),
+    email: this.fb.control('', Validators.compose([Validators.required, Validators.email])),
+    gender: this.fb.control('female'),
+    role: this.fb.control(''),
+    isActive: this.fb.control(false)
+  })
+
+  onSubmit() {
+    if(this.registerForm.valid) {
+      this.service.saveUser(this.registerForm.value).subscribe(res=>{
+        this.toastr.success('Please concatc admin for enableing access','Registered Successfully')
+      })
+    } else {
+      this.toastr.warning('Please enter valid data');
+    }
+  }
+}
---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  apiUrl = 'http://localhost:3000/user'
+  constructor(private http:HttpClient) { }
+
+  getAllUsers() {
+    return this.http.get(this.apiUrl)
+  }
+
+  getByCode(code:any) {
+    return this.http.get(this.apiUrl+'/'+code)
+  }
+
+  saveUser(inputData: any) {
+return this.http.post(this.apiUrl, inputData)
+  }
+
+  updateUser(code: any, inputData: any) {
+    return this.http.put(this.apiUrl+'/'+code, inputData)
+  }
+  
+}
